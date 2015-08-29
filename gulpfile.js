64626052b802d238fc2b0e43c950047ca3416773
Ari Porad <git@ariporad.com>
2015-08-28 17:04:03 -0700

    chore(gulp): setup gulp
    Setup gulp with building, linting, testing and watching (builds, lints, tests, restarts)

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Ari on 8/28/15.
+ */
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var server = require('gulp-develop-server');
+
+var SRC = ['src/**/*.js'];
+var DEST = 'build';
+var TESTS = 'src/**/*.test.js';
+
+gulp.task('default', ['build']);
+
+gulp.task('build', ['test'], function() {
+  return gulp.src(SRC)
+    .pipe(plugins.changed(DEST))
+    .pipe(plugins.babel())
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(SRC)
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], function() {
+  require('babel/register'); // Allow ES6 tests
+  return gulp.src(TESTS)
+    .pipe(plugins.mocha());
+});
+
+gulp.task('watch', ['build', 'server:start'], function() {
+  gulp.watch(SRC, ['build', 'server:restart']);
+});
+
+gulp.task('server:start', function(){
+  server.listen({ path: './src/index.js' });
+});
+
+gulp.task('server:restart', function(){
+  server.restart();
+});
